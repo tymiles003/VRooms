7341b6f6eb822772a379502b803bb381440148b0
unknown <novialim@gmail.com>
2017-09-10 01:31:56 -0700

    Set media query to hide intro video for mobile devices

--- public/js/videoeffect.js
+++ public/js/videoeffect.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$("video").css('opacity', 0).animate( { opacity: 1 }, 'fast');
+	// $("video").css('opacity', 0).animate( { opacity: 1 }, 'fast');
 
 	$('video').bind('ended', function(){
 	   $(this).fadeOut(2000);
