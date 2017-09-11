4a9df8aee7ddddb26535c2b5f37ddb24641969d9
Priyanka Arora <aqupriyanka@gmail.com>
2017-09-10 23:38:09 -0700

    google login production keys

--- controllers/config/auth.js
+++ controllers/config/auth.js
@@ -10,8 +10,19 @@ module.exports = {
     callbackURL: 'https://crossorigin.me/http://localhost:5000/auth/twitter/callback',
   },
   googleAuth: {
-    clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
-    clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
-    callbackURL: 'https://crossorigin.me/http://localhost:5000/auth/google/callback',
+    //FOR DEV
+    // clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
+    // clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
+    // callbackURL: 'https://crossorigin.me/http://localhost:5000/auth/google/callback',
+
+    //FOR DEV KEYS-2
+    // clientID: "430840990935-7u7n4qn2vkibch16l7akc0f67av9b55e.apps.googleusercontent.com",
+    // clientSecret: "aPXCfvct60ACiushw3B4R8hm",
+    // callbackURL: 'http://localhost:3000/auth/google/callback',
+
+    //FOR PROD
+    clientID: "243612288079-f5s6vep0ag5frfluh0l1bng60kkncsu8.apps.googleusercontent.com",
+    clientSecret: "FVQWsYreVVDi1t_VAwV0_7_m",
+    callbackURL: "/auth/google/callback"
   },
 };
