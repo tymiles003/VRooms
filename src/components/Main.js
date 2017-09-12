36b145bfeadfa1532bd250374eae2531b939c2ad
Joseph Huynh <joe.huynh@gmail.com>
2017-09-12 00:05:37 -0700

    Waynestrap too powerful. Rolling back to previous commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
 import LoginModal from "./LoginModal";
-import Navbar from './common/Navbar';
+
 
 require('./styles/Main.css');
 
@@ -34,7 +34,36 @@ class Main extends Component {
                 <source src="https://s3.amazonaws.com/vrooms/splashintro.mp4" type="video/mp4" />
               </video>
 
-							<Navbar logo_filename="VRooms_V10_Hori_Gray" theme="white-bg"/>
+                <div className="navigation navigation--main navigation--main--gradient">
+                  <div className="navigation-wrapper">
+                    <a href="/" className="navigation__logo-link emerge" data-duration="600" data-effect="slide" data-right="64px"  >
+                      <img className="navigation__logo" width="220" src="/assets/img/logo/VRooms_V11_Hori_Gray.png" alt="VRooms" /> 
+                    </a>
+                    <div className="navigation-mobile js-open-menu">
+                      <div className="navigation-mobile__icon js-open-menu-btn"></div>
+                    </div>
+                    <nav className="navigation-menu emerge" data-duration="600" data-effect="slide" data-left="64px">
+                        <a href="/about" className="navigation-menu__link ">ABOUT</a>
+                        <a href="/agents" className="navigation-menu__link ">AGENTS</a>
+                        <a href="/showcase" className="navigation-menu__link ">SHOWCASE</a>
+                        <a href="/contact" className="navigation-menu__link ">CONTACT US</a>
+
+                        <a rel="signup" href="/signup" className="navigation-menu__link navigation-menu__link--hidden navigation-menu__link--lng a-signup">sign up</a>
+                        <a data-auth="no" href="/login" className="navigation-menu__sign_in a-login">sign in</a>
+                    </nav>
+                  </div>
+                </div>
+
+                <div className="mobile-menu js-mobile-menu">
+                    <a href="/about" className="mobile-menu__link text--white text--bold a-submenu" data-page-name="Agents">About</a>
+                    <a href="/agents" className="mobile-menu__link text--white text--bold a-submenu" data-page-name="Agents">Agents</a>
+                    <a href="/showcase" className="mobile-menu__link text--white text--bold ">Showcase</a>
+                    <a href="/contact" className="mobile-menu__link text--white text--bold ">Contact us</a>
+
+                  <div className="mobile-menu-btn-wrapper">
+                      <a data-auth="no" href="https://watch.appfollow.io?ref=github.com" className="mobile-menu__btn navigation-menu__sign_in a-login">sign in</a>
+                  </div>
+                </div>
 
                 <header className="header header--main js-header a-page" data-landing="yes" data-page="Home" data-page-name="Main">
                   <div className="description">
