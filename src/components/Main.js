2ba93c3883214f8e3e3450be1084604099b9e23a
Wayne Cheng <wayne@che.ng>
2017-09-13 20:46:57 -0700

    sorted out stylesheet mess. changed each page to const rather than class. moved Auth component from Main.js to Navbar component, so it's located right next to login button and available on every page. changed Navbar component to class with states in order to handle Auth component. removed CSS file imports from Main.js and and About.js, which was flooding everywhere.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,98 +1,124 @@
-import React, { Component } from "react";
-import {Helmet} from "react-helmet";
+// import React, { Component } from "react";
+import React from "react";
+import { Helmet } from "react-helmet";
 // import { Link } from "react-router";
 // import Navbar from "./common/Navbar";
 // import Footer from "./common/Footer";
-import LoginForm from "./authentication/LoginForm";
+import "aframe";
+import "aframe-animation-component";
+import "aframe-particle-system-component";
+import "babel-polyfill";
 
-import 'aframe';
-import 'aframe-animation-component';
-import 'aframe-particle-system-component';
-import 'babel-polyfill';
-import Modal from "./common/CustomModal";
+// Authentication imports
+// import LoginForm from "./authentication/LoginForm";
+// import Modal from "./common/CustomModal";
 
-import Navbar from './common/Navbar';
-import MobileMenu from './common/MobileMenu';
+import Navbar from "./common/Navbar";
+import MobileMenu from "./common/MobileMenu";
 
-require('./styles/Main.css');
+// require('./styles/Main.css');
 
-class Main extends Component {
-  constructor(props){
-    super(props);
+// class Main extends Component {
+// constructor(props){
+//   super(props);
 
-    this.state={
-      signInClicked:false
-    }
-  }
+//   this.state={
+// 	signInClicked:false
+//   }
+// }
+// this.state = {
+// 	signInClicked:false
+// }
 
-handleAuth = (event) =>{
-  event.preventDefault();
-  console.log("inside handle auth");
-  this.setState({signInClicked:true});
-  
-  // this.forceUpdate();
-}
+// handleAuth = (event) =>{
+// 	event.preventDefault();
+// 	console.log("inside handle auth");
+// 	this.setState({signInClicked:true});
 
+// 	// this.forceUpdate();
+// }
+const Main = props => {
+	//   render() {
+	return (
+		<div className="application">
+			<Helmet>
+				{/* customized script elements */}
+				<script src="./js/drift.js" type="text/javascript" />
 
-  render() {
-      return (
+				{/* CSS links for this page */}
+				<link rel="stylesheet" href="/css/pagestyle.css" />
+				<link href="/src/css/pages/Main.css" rel="stylesheet" />
+				<link rel="stylesheet" href="/css/panning.css" />
+			</Helmet>
 
-        <div className="application">
-            <Helmet>
-              {/* customized script elements */}
-              <script src="./js/drift.js" type="text/javascript" />
-                
-            </Helmet>
-            
-          <div className="wrapper">
+			<div className="wrapper">
+				<video
+					className="video-container video-container-overlay"
+					id="video-background"
+					autoPlay="true"
+				>
+					<source
+						src="./assets/video/splashintro.mp4"
+						type="video/mp4"
+					/>
+				</video>
 
-              <video className="video-container video-container-overlay" id="video-background" autoPlay="true">
-                <source src="./assets/video/splashintro.mp4" type="video/mp4" />
-              </video>
+				<Navbar
+					logo_filename="VRooms_V11_Hori_Gray"
+					theme="opaque-white-bg"
+				/>
+				<MobileMenu />
 
-                <Navbar logo_filename="VRooms_V11_Hori_Gray" theme="white-bg" handleAuth={this.handleAuth}/>
-                {this.state.signInClicked ? 
-                  (<Modal ref={(node) => {this.modal=node;}} modalIsOpen={true} title="Login" >
-                    <LoginForm />
-                    </Modal>
-                  ) : null}
-                <MobileMenu />
-
-                <header className="header header--main js-header a-page" data-landing="yes" data-page="Home" data-page-name="Main">
-                  <div className="description">
-                    <h1 className="description__headline text--white emerge" data-duration="600" data-effect="slide" data-down="64px" data-hold="100">
-                      Virtual Reality for Real Estate
-                    </h1>
-                    <span className="description__sub_headline text--white emerge" data-duration="600" data-effect="slide" data-down="64px" data-hold="100" data-continue="true">
-                      Use VR to <strong>win</strong> more listings and <br />
-                      <strong>stay ahead</strong> of your competition
-                    </span>
-                      
-                  </div>
-
-                </header>
-
-          </div>
-        </div>   
-      );
-  }    
-}
+				<header
+					className="header header--main js-header a-page"
+					data-landing="yes"
+					data-page="Home"
+					data-page-name="Main"
+				>
+					<div className="description">
+						<h1
+							className="description__headline text--white emerge"
+							data-duration="600"
+							data-effect="slide"
+							data-down="64px"
+							data-hold="100"
+						>
+							Virtual Reality for Real Estate
+						</h1>
+						<span
+							className="description__sub_headline text--white emerge"
+							data-duration="600"
+							data-effect="slide"
+							data-down="64px"
+							data-hold="100"
+							data-continue="true"
+						>
+							Use VR to <strong>win</strong> more listings and{" "}
+							<br />
+							<strong>stay ahead</strong> of your competition
+						</span>
+					</div>
+				</header>
+			</div>
+		</div>
+	);
+};
+// }
 
 export default Main;
 
-
-    // <Navbar />
-    // {props.children}
-    // <Footer />
+// <Navbar />
+// {props.children}
+// <Footer />
 
 // ================================================================================
 
-     // Navigation link code for content refresh within same page
-     // <ul className="nav navbar-nav">
-     //    <li className={location.pathname === "/" && "active"}>
-     //      <Link to="/">Home</Link>
-     //    </li>
-     //    <li className={location.pathname === "/favorites" && "active"}>
-     //      <Link to="/favorites">Favorites</Link>
-     //    </li>
-     //  </ul>
+// Navigation link code for content refresh within same page
+// <ul className="nav navbar-nav">
+//    <li className={location.pathname === "/" && "active"}>
+//      <Link to="/">Home</Link>
+//    </li>
+//    <li className={location.pathname === "/favorites" && "active"}>
+//      <Link to="/favorites">Favorites</Link>
+//    </li>
+//  </ul>
