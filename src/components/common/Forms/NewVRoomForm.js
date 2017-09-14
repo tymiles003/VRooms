4c9bb1bfe664249780d87414c336a4154f439690
Wayne Cheng <wayne@che.ng>
2017-09-14 01:38:28 -0700

    lots of uneccesary sass work. form style update

--- src/components/common/Forms/NewVRoomForm.js
+++ src/components/common/Forms/NewVRoomForm.js
@@ -28,16 +28,101 @@ class NewVRoomForm extends Component {
 		});
 	};
 
+	handleScrape = event => {
+		const zillow_url = event.target.value.trim();
+		
+	}
+
 	handleFormSubmit = event => {
 		// Preventing the default behavior of the form submit (which is to refresh the page)
 		event.preventDefault();
 
-		let { topic, startYear, endYear } = this.state;
+		let { agent, street, city, state, zip } = this.state;
+		console.log('this.state',this.state);
 	};
 
 	render() {
 		return (
+			<div>
 				<form id="new-vroom-form" className="form ws-form">
+					<div className="form-row">
+						<div className="input-wrap input-full-width input-street ws-input-wrap">
+							{/* <label for="street">Street</label> */}
+							<input
+								id="zillow_url"
+								className="input ws-input"
+								type="text"
+								name="zillow_url"
+								placeholder="Zillow URL"
+								onChange={this.handleScrape}
+							/>
+						</div>
+					</div>
+					<div className="form-row">
+						<fieldset>
+							<legend>Address</legend>
+							<div className="form-field-row">
+								<div className="input-wrap input-full-width input-street ws-input-wrap">
+									{/* <label for="street">Street</label> */}
+									<input
+										id="street"
+										className="input ws-input"
+										type="text"
+										name="street"
+										placeholder="Street"
+										onChange={this.handleInputChange}
+									/>
+								</div>
+							</div>
+							<div className="form-field-row">
+								<div className="input-wrap input-city validated">
+									{/* <label for="city">City</label> */}
+									<input
+										id="city"
+										className="input ws-input"
+										type="text"
+										name="city"
+										placeholder="City"
+										onChange={this.handleInputChange}
+									/>
+								</div>
+								<div className="input-wrap input-state error">
+									{/* <label for="state">State</label> */}
+									<input
+										id="state"
+										className="input ws-input"
+										type="text"
+										name="state"
+										placeholder="State"
+										onChange={this.handleInputChange}
+									/>
+								</div>
+								<div className="input-wrap input-zip">
+									{/* <label for="zip">Zip</label> */}
+									<input
+										id="zip"
+										className="input ws-input"
+										type="text"
+										name="zip"
+										placeholder="Zip"
+										onChange={this.handleInputChange}
+									/>
+								</div>
+							</div>
+						</fieldset>
+					</div>
+					<div className="form-row">
+						<button
+							id="submit"
+							className="ws-btn"
+							type="submit"
+							onClick={this.handleFormSubmit}
+						>
+						Submit
+						</button>
+					</div>
+				</form>
+				{/* <form className="naked-form">
 					<fieldset>
 						<legend>Address</legend>
 						<div className="form-field-row">
@@ -54,7 +139,7 @@ class NewVRoomForm extends Component {
 						<div className="form-field-row">
 							<div className="input-wrap input-city">
 								<input
-									className="input ws-input"
+									className="input ws-input validated"
 									type="text"
 									placeholder="City"
 									name="city"
@@ -63,7 +148,7 @@ class NewVRoomForm extends Component {
 							</div>
 							<div className="input-wrap input-state">
 								<input
-									className="input ws-input"
+									className="input ws-input error"
 									type="text"
 									placeholder="State"
 									name="state"
@@ -81,7 +166,8 @@ class NewVRoomForm extends Component {
 							</div>
 						</div>
 					</fieldset>
-				</form>
+				</form> */}
+			</div>
 		)
 	}
 }
