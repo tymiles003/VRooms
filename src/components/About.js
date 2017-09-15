a44d7e2179d2779b86bdd657b6d594af21e76ab4
unknown <novialim@gmail.com>
2017-09-14 19:29:34 -0700

    About page MVP

--- src/components/About.js
+++ src/components/About.js
@@ -41,7 +41,9 @@ const About = (props) => {
                         <div className="small-12 large-6 columns feature-content left">
                           <div className="feature-text">
                             <h3>Engage</h3>
-                            <h5>Say hello to the next generation of homebuying. Engage buyers by providing the most immersive and realistic way to view listings on web, mobile, and virtual reality.</h5>
+                            <h5>
+                                Say hello to the next generation of homebuying. <br />
+                                Engage buyers by providing the most immersive and realistic way to view listings on web, mobile, and virtual reality.</h5>
                             <hr className="benefit-divider-1" />
                           </div>
                         </div>
@@ -64,7 +66,53 @@ const About = (props) => {
                       
                     </div>
                   </section>
+
+                  <section className="benefits-header cta-module">
+                    <div className="row expanded">
+                      <div className="small-12 medium-12 columns cta-content">
+                        <h3>Be the Market Leader! Get VRooms!</h3>
+                        <a className="button" href="/login" id="marketing-signup">Sign Up</a>
+                      </div>
+                    </div>
+                  </section>
+
+                  <section className="testimonials">
+                  <div className="row expanded">
+                    <div className="small-centered small-12 columns">
+                      <h4>What Our Founding Team Is Excited About</h4>
+                      <div className="row small-up-1 medium-up-4">
+                      <div className="column column-block">
+                          <img className="float-center customer-img" src="/assets/img/team/novia.png" alt="Novia Lim" />
+                          <h6>“We are really excited to make virtual reality accessible from novices to tech savvy. VRooms breaks down the restrictions of a traditional home buying process and revolutionize the real estate industry. Join us now to increase your sales efficiency and maximise your customer engagement!”</h6>
+                          <p><strong>Novia Lim-Sampson</strong></p>
+                          <p>Founder & CEO</p>
+                          
+                        </div>
+                        <div className="column column-block">
+                          <img className="float-center customer-img" src="/assets/img/team/wayne.png" alt="Wayne Cheng" />
+                          <h6>“Lucas ipsum dolor sit amet chewbacca organa fisto yoda cade mandalore thrawn bothan organa dantooine. Maul calamari skywalker antilles windu ahsoka gonk. Organa organa watto moff calamari kashyyyk mon hutt grievous. Organa padmé twi'lek twi'lek moff darth qui-gon. Palpatine jawa endor mace mon moff.”</h6>
+                          <p><strong>Wayne Cheng</strong></p>
+                          <p>Founding Partner & COOL Wannabe</p>
+                        </div>
+                        <div className="column column-block">
+                          <img className="float-center customer-img" src="/assets/img/team/joseph.png" alt="Joseph Huynh" />
+                          <h6>“Lucas ipsum dolor sit amet chewbacca organa fisto yoda cade mandalore thrawn bothan organa dantooine. Maul calamari skywalker antilles windu ahsoka gonk. Organa organa watto moff calamari kashyyyk mon hutt grievous. Organa padmé twi'lek twi'lek moff darth qui-gon. Palpatine jawa endor mace mon moff.”</h6>
+                          <p><strong>Joseph Huynh</strong></p>
+                          <p>Founding Partner & CTO</p>
+                        </div>
+                        <div className="column column-block">
+                          <img className="float-center customer-img" src="/assets/img/team/priyanka.png" alt="Priyanka Arora" />
+                          <h6>“Lucas ipsum dolor sit amet chewbacca organa fisto yoda cade mandalore thrawn bothan organa dantooine. Maul calamari skywalker antilles windu ahsoka gonk. Organa padmé twi'lek twi'lek moff darth qui-gon. Palpatine jawa endor mace mon moff.”</h6>
+                          <p><strong>Priyaka Arora</strong></p>
+                          <p>Founding Partner & COO</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
                 </div>  
+
+                
 
                 
 
