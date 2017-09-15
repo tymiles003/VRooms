5c686f250b454aa4fd53cfc946949e1cff9f2362
unknown <novialim@gmail.com>
2017-09-15 13:04:35 -0700

    removed react-fade dep

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -1,20 +1,12 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
-import { default as Fade } from 'react-fade'
 
 import Navbar from "../common/Navbar";
-
-const fadeDuration = 10
 
 // const Navbar = (this.props) => (
 class MainContent extends Component {
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			fadeOut: false,
-			visibility: 'visible'
-		}
 	}
 
 	componentDidMount(){
@@ -27,75 +19,59 @@ class MainContent extends Component {
             }, 2000)
           }, 1000)
 		}
-
-		setTimeout(() => {
-            this.setState({ fadeOut: true })
-          }, 1000)
     }
 
 	render() {
 
 		return (
-			<div className="wrapper">
+			<div className="wrapper">			
 
-				<Fade
-					out={this.state.fadeOut}
-					duration={fadeDuration}
-					style={{
-						visibility: this.state.visibility
-					}}
-					>
-					
-					
-
-					<video
-						className="video-container video-container-overlay"
-						id="video-background"
-						autoPlay="true"
-					>
-						<source
-							src="./assets/video/splashintro.mp4"
-							type="video/mp4"
-						/>
-					</video>
-
-					<Navbar
-						logo_filename="VRooms_V11_Hori_Gray"
-						theme="opaque-white-bg"
+				<video
+					className="video-container video-container-overlay"
+					id="video-background"
+					autoPlay="true"
+				>
+					<source
+						src="./assets/video/splashintro.mp4"
+						type="video/mp4"
 					/>
+				</video>
 
-					<header
-						className="header header--main js-header a-page"
-						data-landing="yes"
-						data-page="Home"
-						data-page-name="Main"
-					>
-						<div className="description">
-							<h1
-								className="description__headline text--white emerge"
-								data-duration="600"
-								data-effect="slide"
-								data-down="64px"
-								data-hold="100"
-							>
-								Virtual Reality for Real Estate
-							</h1>
-							<span
-								className="description__sub_headline text--white emerge"
-								data-duration="600"
-								data-effect="slide"
-								data-down="64px"
-								data-hold="100"
-								data-continue="true"
-							>
-								Use VR to <strong>win</strong> more listings and{" "}
-								<br />
-								<strong>stay ahead</strong> of your competition
-							</span>
-						</div>
-					</header>
+				<Navbar
+					logo_filename="VRooms_V11_Hori_Gray"
+					theme="opaque-white-bg"
+				/>
 
-				</Fade>	
+				<header
+					className="header header--main js-header a-page"
+					data-landing="yes"
+					data-page="Home"
+					data-page-name="Main"
+				>
+					<div className="description">
+						<h1
+							className="description__headline text--white emerge"
+							data-duration="600"
+							data-effect="slide"
+							data-down="64px"
+							data-hold="100"
+						>
+							Virtual Reality for Real Estate
+						</h1>
+						<span
+							className="description__sub_headline text--white emerge"
+							data-duration="600"
+							data-effect="slide"
+							data-down="64px"
+							data-hold="100"
+							data-continue="true"
+						>
+							Use VR to <strong>win</strong> more listings and{" "}
+							<br />
+							<strong>stay ahead</strong> of your competition
+						</span>
+					</div>
+				</header>	
 			</div>
 		)
 	}
