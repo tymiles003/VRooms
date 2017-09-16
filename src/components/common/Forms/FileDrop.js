c1f22ea2ef2a3c85fbd4659f05d4f4f186d48263
Wayne Cheng <wayne@che.ng>
2017-09-16 10:46:16 -0700

    FileDrop upgrades

--- src/components/common/Forms/FileDrop.js
+++ src/components/common/Forms/FileDrop.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import API from "../../../utils/API";
-import Dropzone from 'react-dropzone';
+import Dropzone from "react-dropzone";
 
 class FileDrop extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			file: '',
-			bits: '',
-			fileStatus: 'no-file',
+			file: "",
+			bits: "",
+			fileStatus: "no-file"
 		};
 	}
-	
+
 	handleInputChange = event => {
 		event.preventDefault();
 		const value = event.target.value;
@@ -22,11 +22,11 @@ class FileDrop extends Component {
 		this.setState({
 			[name]: value
 		});
-	}
+	};
 
 	onDrop = acceptedFiles => {
-		console.log('>>> onDrop');
-		console.log('acceptedFiles.length',acceptedFiles.length);
+		console.log(">>> onDrop");
+		console.log("acceptedFiles.length", acceptedFiles.length);
 		const file = acceptedFiles[0];
 
 		const reader = new FileReader();
@@ -36,42 +36,49 @@ class FileDrop extends Component {
 
 			this.setState({
 				bits: bits,
-				fileStatus: 'photo-ready'
-			})
+				fileStatus: "photo-ready"
+			});
 		};
-		reader.onabort = () => console.log('file reading was aborted');
-		reader.onerror = () => console.log('file reading has failed');
-		if (file) { reader.readAsDataURL(file); }
-	}
+		reader.onabort = () => console.log("file reading was aborted");
+		reader.onerror = () => console.log("file reading has failed");
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+	};
 
-	
 	render() {
 		return (
 			<div className="filedrop-wrap">
-				<Dropzone 
-					onDrop={this.onDrop.bind(this)}
-					className="dropzone"
-				>
+				<Dropzone onDrop={this.onDrop.bind(this)} className="dropzone">
 					<div className="dropzone-content">
 						<div className="feature-icon">
 							{/* <i className="fa fa-picture-o"></i> */}
-							<img className='img-icon' src="/assets/graphics/360-photo-o-black.svg"></img>
+							<img
+								className="img-icon"
+								src="/assets/graphics/360-photo-o-black.svg"
+							/>
 							{/* <img className='img-icon' src="/assets/graphics/360-photo-black.svg"></img> */}
 							{/* <img className='img-icon' src="/assets/graphics/360-photo-white.svg"></img> */}
 						</div>
 						<div className="direction-wrap">
-							<h4 className="direction-headline"> Drag & Drop </h4>
-							<p className="direction-subheadline">or click to browse your files.</p>
+							<h4 className="direction-headline">
+								{" "}
+								Drag & Drop{" "}
+							</h4>
+							<p className="direction-subheadline">
+								or click to browse your files.
+							</p>
 						</div>
 					</div>
 				</Dropzone>
-				<figure 
+				{this.state.files.map(f => ( <span key={f.name}> {f.name} - {f.size} bytes </span> ))}
+				<figure
 					id="filedrop-preview"
-					className={"img-canvas "+ this.state.fileStatus}
-					style={{backgroundImage: `url("${this.state.bits}")`}}
-				></figure>
+					className={"img-canvas " + this.state.fileStatus}
+					style={{ backgroundImage: `url("${this.state.bits}")` }}
+				/>
 			</div>
-		)
+		);
 	}
 }
 
