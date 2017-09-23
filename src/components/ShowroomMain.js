4311f11d7d53e03082c3967a9dce4c292ab897c1
Joseph Huynh <joe.huynh@gmail.com>
2017-09-22 19:35:48 -0700

    Now passing property to Showroom component

--- src/components/ShowroomMain.js
+++ src/components/ShowroomMain.js
@@ -4,46 +4,49 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Entity, Scene } from "aframe-react";
 import { Helmet } from "react-helmet";
-import Showroom from "./Showroom"
+import Showroom from "./Showroom";
 
 import propertyAPI from "../utils/propertyAPI";
 
 // Showcasing gallery list of real estate
 class ShowroomMain extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    //init states
-    this.state = {
-      selectedProperty: [],
-      room_url: ""
+        //init states
+        this.state = {
+            selectedProperty: [],
+            room_url: ""
+        };
+    }
+
+    componentWillMount() {
+        // To pass/retrieve id from showcase gallery
+        //this.getSelectedProperty("59c53629778a8b0012433cf4");
+
+        // Set property using location object passed from <Link>
+        console.log("this.props.location: ", this.props.location);
+        console.log("this.props.location.state.property: ", this.props.location.property);
+        this.setState({
+            selectedProperty: this.props.location.state.property
+        });
+    }
+
+    getSelectedProperty = propID => {
+        propertyAPI.getProperty(propID).then(response => {
+            this.setState({
+                selectedProperty: response.data,
+                room_url: response.data[0].thumbnail_url
+            });
+        });
     };
-  }
 
-  componentWillMount() {
-    // To pass/retrieve id from showcase gallery
-    this.getSelectedProperty("59c53629778a8b0012433cf4");
-  }
+    componentDidMount = () => {
+    };
 
-  getSelectedProperty = propID => {
-    propertyAPI.getProperty(propID).then(response => {
-
-      this.setState({
-        selectedProperty: response.data, 
-        room_url:response.data[0].thumbnail_url
-      });
-    });
-  };
-
-  componentDidMount = () => {
-  };
-
-  render() {
-    return (    
-
-      <Showroom selectedProperty={this.state.selectedProperty}/>
-    );
-  }
+    render() {
+        return <Showroom selectedProperty={this.state.selectedProperty} />;
+    }
 }
 
 export default ShowroomMain;
