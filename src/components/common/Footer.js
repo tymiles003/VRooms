0643b3a46a057ef9da449738a4936c40bbdf5e67
unknown <novialim@gmail.com>
2017-09-23 23:20:56 -0700

    mobile nav sign in styling, mobile sign up form padding adjustments, mobile footer styling updates

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -14,7 +14,7 @@ class Footer extends Component {
       <div className="footer-navigation">
         <div className="row">
           <div className="small-12 medium-12 large-10 medium-centered columns">
-            <div className="medium-5 columns footer--column">
+            <div className="medium-5 columns footer--column footer--row">
               <h5>Get VRooms</h5>
               <ul>
               <li><a id="ga-footer-about" href="/about">About</a></li>
@@ -24,7 +24,7 @@ class Footer extends Component {
               </ul>
             </div>
   
-            <div className="medium-5 columns footer--column">
+            <div className="medium-5 columns footer--column footer--row">
               <h5>Get in Touch</h5>
               <ul>
                 <li><a id="ga-footer-contact" href="/contact">Contact</a></li>
@@ -43,10 +43,12 @@ class Footer extends Component {
       <div className="footer-navigation text-center">
         <strong>©2017 VRooms, Inc.</strong> 
         <br />
-        Made with love by <a target="_blank" href="https://github.com/novialim">Novia Lim</a>&nbsp;|&nbsp; 
-        <a target="_blank" href="https://github.com/wayncheng">Wayne Cheng</a>&nbsp;|&nbsp; 
-        <a target="_blank" href="https://github.com/jhuynh85">Joseph Huynh</a>&nbsp;|&nbsp; 
-        <a target="_blank" href="https://github.com/aqupriyanka">Priyaka Arora</a>
+        <div className="team-github">
+          Made with love by <a target="_blank" href="https://github.com/novialim">Novia Lim</a>&nbsp;|&nbsp; 
+          <a target="_blank" href="https://github.com/wayncheng">Wayne Cheng</a>&nbsp;|&nbsp; 
+          <a target="_blank" href="https://github.com/jhuynh85">Joseph Huynh</a>&nbsp;|&nbsp; 
+          <a target="_blank" href="https://github.com/aqupriyanka">Priyaka Arora</a>
+        </div>
       </div>
     </footer>        
 		)
