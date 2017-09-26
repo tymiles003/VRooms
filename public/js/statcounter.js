3d7d88e0538e0f07c48d8c243e8999a4d132431a
Joseph Huynh <joe.huynh@gmail.com>
2017-09-25 23:55:13 -0700

    Updated packages. Added Statcounter script

---
+++ public/js/statcounter.js
@@ -0,0 +1,12 @@
+let sc_project = 11456234;
+let sc_invisible = 1;
+let sc_security = "1ce074aa";
+let scJsHost =
+    "https:" == document.location.protocol ? "https://secure." : "http://www.";
+document.write(
+    "<sc" +
+        "ript type='text/javascript' src='" +
+        scJsHost +
+        "statcounter.com/counter/counter.js'></" +
+        "script>"
+);
