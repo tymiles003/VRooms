7506b97fad6c2e79fb9f915a120725b3da4d479c
Joseph Huynh <joe.huynh@gmail.com>
2017-09-28 19:19:40 -0700

    Fixed socket code

--- src/components/ShowroomMain.js
+++ src/components/ShowroomMain.js
@@ -25,24 +25,10 @@ class ShowroomMain extends React.Component {
             room_url: "",
             pano_url: "",
             annotations: [],
-            message: 1
+            message: 1,
+            roomID: ""
         };
     }
-
-    // componentWillMount() {
-    // 	// To pass/retrieve id from showcase gallery
-    // 	//this.getSelectedProperty("59c53629778a8b0012433cf4");
-
-    // 	// Set property using location object passed from <Link>
-    // 	console.log("this.props.location: ", this.props.location);
-    // 	console.log(
-    // 		"this.props.location.state.property: ",
-    // 		this.props.location.state.property
-    // 	);
-    // 	this.setState({
-    // 		selectedProperty: this.props.location.state.property
-    // 	});
-    // }
 
     // componentDidMount ===============================
     componentDidMount = () => {
@@ -76,15 +62,6 @@ class ShowroomMain extends React.Component {
     portAframe = aframeState => {
         this.setState(aframeState);
     };
-    // getSelectedProperty =====================
-    getSelectedProperty = propID => {
-        propertyAPI.getProperty(propID).then(response => {
-            this.setState({
-                selectedProperty: response.data,
-                room_url: response.data[0].thumbnail_url
-            });
-        });
-    };
 
     componentWillMount() {
         let rID;
@@ -96,6 +73,9 @@ class ShowroomMain extends React.Component {
         } else {
             rID = propsID;
         }
+        this.setState({
+            roomID: rID
+        });
 
         if (rID) {
             console.log("rID", rID);
@@ -144,91 +124,3 @@ class ShowroomMain extends React.Component {
 }
 
 export default ShowroomMain;
-
-//   constructor(props) {
-//       super(props);
-
-//       //init states
-//       this.state = {
-//           selectedProperty: [],
-//           room_url: "",
-//           message:1
-//       };
-//   }
-
-//   componentDidMount() {
-//   /**
-//    * While browser tab getting closed, below event listener is called.
-//    */
-//   window.addEventListener("unload", (ev) => {
-//     ev.preventDefault();
-
-//     /**
-//      * Close a connection, if any browser tab is closed or page is refreshed.
-//      */
-//     socket.emit('close', {
-//         room: this.props.location.state.property.rooms[0].pano_url
-//       });
-//     });
-
-//   //   document.addEventListener("pagehide", (ev) => {
-//   //   ev.preventDefault();
-
-//   //   /**
-//   //    * Close a connection, if any browser tab is closed or page is refreshed.
-//   //    */
-//   //   socket.emit('close', {
-//   //       room: this.props.location.state.property.rooms[0].pano_url
-//   //     });
-//   //   });
-
-//     socket.emit('open', {room: this.props.location.state.property.rooms[0].pano_url});
-
-//     /**
-//      * Message reeived from server
-//      */
-//     socket.on('message', (data) => {
-//         console.log("Message ==== ",data);
-//         this.setState({message:data});
-//     });
-// }
-
-// componentWillMount() {
-//     // To pass/retrieve id from showcase gallery
-//     //this.getSelectedProperty("59c53629778a8b0012433cf4");
-
-//     // Set property using location object passed from <Link>
-//     console.log("this.props.location: ", this.props.location);
-//     console.log("this.props.location.state.property: ", this.props.location.state.property);
-//     // this.setState({
-//     //     selectedProperty: this.props.location.state.property
-//     // });
-// }
-
-//   getSelectedProperty = propID => {
-//       propertyAPI.getProperty(propID).then(response => {
-//           this.setState({
-//               selectedProperty: response.data,
-//               room_url: response.data[0].thumbnail_url
-//           });
-//       });
-//   };
-
-//   componentWillUnmount(){
-//     socket.emit('close', {
-//         room: this.props.location.state.property.rooms[0].pano_url
-//       });
-// }
-
-// render() {
-//     return (
-//         <span>
-//             <span style={{position:"absolute", bottom:0,zIndex:1000, left:0}}>Number of People Viewing this Property: {this.state.message}</span>
-//             <Showroom
-//             selectedProperty={(this.props.location.state.property) ? (this.props.location.state.property) : ""}
-//             message={this.state.message}
-//             />
-//         </span>
-//     );
-// }
-// return <Showroom selectedProperty={(this.props.location.state.property) ? (this.props.location.state.property) : ""} />;
