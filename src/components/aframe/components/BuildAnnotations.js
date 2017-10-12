5f5ffc9a95e6a65e0a52e43ec5a78c305df059b6
wayne <wayne@che.ng>
2017-10-11 18:22:45 -0700

    init integration of Edit Mode

--- src/components/aframe/components/BuildAnnotations.js
+++ src/components/aframe/components/BuildAnnotations.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import Annotation from "./Annotation";
 import AnnoLink from "./AnnoLink";
 import ConstAnnoLink from "./ConstAnnoLink";
 import { Entity } from "aframe-react";
 
-const BuildAnnotations = ( props ) => {
+const BuildAnnotations = props => {
 	// const annos = props.annotations;
 	// console.log('annos',annos);
 	let annotationsToBuild = props.annotations;
@@ -14,21 +14,28 @@ const BuildAnnotations = ( props ) => {
 	return (
 		<Entity>
 			{annotationsToBuild.map((ea, index) => {
-				{/* console.log('ea',ea); */}
-				{/* console.log('ea.link',ea.link); */}
-				{/* if ( ea.link !== '' ) { */}
-				if ( ea.link ) {
-					{/* return <ConstAnnoLink data={ ea } key={ index } /> */}
-					return <AnnoLink location={props.location} history={props.history} data={ ea } key={ index } />
-					{/* let LinkRouter = withRouter(AnnoLink); */}
-					{/* return <LinkRouter  data={ea}  key={index} />; */}
-				}
+				if (ea.link) {
+					return (
+					<AnnoLink
+						location={props.location}
+						history={props.history}
+						data={ea}
+						key={index}
+						inEditMode={this.props.inEditMode}
+						/>
+					);
+				} 
 				else {
-					return <Annotation data={ea} key={index}/>;
+					return (
+					<Annotation 
+						data={ea} 
+						key={index}
+						inEditMode={this.props.inEditMode}
+						/>
+					)
 				}
 			})}
 		</Entity>
 	);
 };
 export default withRouter(BuildAnnotations);
-
